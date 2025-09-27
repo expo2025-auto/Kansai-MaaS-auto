99d7876ac610d1f91ed973670d8a3582ec0b5ba1
expo2025-auto <danshicook@gmail.com>
2025-09-28 02:23:44 +0900

    Update change-auto.js

--- change-auto.js
+++ change-auto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kansai-MaaS 予約時間変更オート（前/後
 // @namespace    https://example.com/na
-// @version      1.0
+// @version      1.1
 // @description  関西MaaSの予約時間を前後にずらす
 // @match        https://app.kansai-maas.jp/ticket-managements*
 // @run-at       document-idle
